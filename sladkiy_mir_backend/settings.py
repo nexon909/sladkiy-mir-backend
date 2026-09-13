3689b9c9cabd8b46bec14242d00960a608310fb3
nexon909 <lunaticamirnelunatic@gmail.com>
2026-09-13 13:08:21 +0500

    fix: add whitenoise to middleware

--- sladkiy_mir_backend/settings.py
+++ sladkiy_mir_backend/settings.py
@@ -55,16 +55,7 @@ INSTALLED_APPS = [
     'api',
 ]
 
-MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware', # В самом верху!
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+
 
 
 
@@ -145,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
